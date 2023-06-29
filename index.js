3e2b19edce412539cae08cc3b50f86e8fa07e8e0
cittasottoscaccosrl <85392183+cittasottoscaccosrl@users.noreply.github.com>
2023-06-29 17:16:33 +0200

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let map, heatmap;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,
+    zoom: 2,
     center: { lat: 40.68591152915883, lng: 14.528731882178715 },
     mapTypeId: "satellite",
   });
